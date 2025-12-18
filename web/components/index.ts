9637df9246531f9da9f14a1dc5c6af2c46e1e9ed
Jermaine Merritt <your-new-email@example.com>
2025-12-18 03:19:36 -0500

    Fix: Use named exports instead of default exports in components/index.ts

--- web/components/index.ts
+++ web/components/index.ts
@@ -1,4 +1,4 @@
-export { default as AnnotationArchive } from './annotation-archive';
-export { default as AnnotationExport } from './annotation-export';
-export { default as AnnotationForm } from './annotation-form';
-export { default as AnnotationTimeline } from './annotation-timeline';
+export { AnnotationArchive } from './annotation-archive';
+export { AnnotationExport } from './annotation-export';
+export { AnnotationForm } from './annotation-form';
+export { AnnotationTimeline } from './annotation-timeline';
