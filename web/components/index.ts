42433ba457e2ed7b3d0fd5792e3e00115483ce29
Jermaine Merritt <your-new-email@example.com>
2025-12-18 02:21:22 -0500

    Add barrel exports for better module resolution on Render

---
+++ web/components/index.ts
@@ -0,0 +1,4 @@
+export { default as AnnotationArchive } from './annotation-archive';
+export { default as AnnotationExport } from './annotation-export';
+export { default as AnnotationForm } from './annotation-form';
+export { default as AnnotationTimeline } from './annotation-timeline';
